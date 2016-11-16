42fc47d28b96dd03bebc05a9b37315cc7f295860
likelion_jh <dlaem@naver.com>
2016-11-16 20:33:30 +0000

    페이스북 연동 직전

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
+  def self.find_for_facebook_oauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.name = auth.info.name   # assuming the user model has a name
+    user.image = auth.info.image # assuming the user model has an image
+  end
+
+  # 이 때는 이상하게도 after_create 콜백이 호출되지 않아서 아래와 같은 조치를 했다.
+  user.add_role :user if user.roles.empty?
+  user   # 최종 반환값은 user 객체이어야 한다.
+  end
+
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -18,18 +34,5 @@ class User < ActiveRecord::Base
   
 end
 
-  def self.find_for_facebook_oauth(auth)
-    user = where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
-  end
 
-  # 이 때는 이상하게도 after_create 콜백이 호출되지 않아서 아래와 같은 조치를 했다.
-  user.add_role :user if user.roles.empty?
-  user   # 최종 반환값은 user 객체이어야 한다.
-  end
 
